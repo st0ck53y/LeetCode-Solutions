4c61a47f5ef3130bcaecda40ebacccedb1c4a2d5
tom <thomas@stocks-family.com>
2016-03-14 11:31:18 +0000

    Finish #6

--- Leetcode/src/com/st0ck53y/leetcode/six/Solution.java
+++ Leetcode/src/com/st0ck53y/leetcode/six/Solution.java
@@ -2,6 +2,36 @@ package com.st0ck53y.leetcode.six;
 
 public class Solution {
 	public String convert(String s, int numRows) {
-        return "";
+		if (s == null || s.equals("")) return "";
+		if (numRows < 2) return s;
+        String[] zig = new String[numRows];
+        for (int i = 0; i < numRows; i++) {
+        	zig[i] = ""; //initialise array
+        }
+        int cr = 0; //current row of zig
+        boolean up = false; //direction of zig
+        for (int i = 0; i < s.length(); i++) {
+        	zig[cr] += s.charAt(i);
+        	if (up) {
+        		if (cr == 0) {
+        			up = false;
+        			cr++;
+        		} else {
+        			cr--;
+        		}
+        	} else {
+	        	if (cr == (numRows - 1)) {
+	        		up = true;
+	        		cr--;
+	        	} else {
+	        		cr++;
+	        	}
+        	}
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numRows; i++) {
+        	sb.append(zig[i]); //compile into single string
+        }
+        return sb.toString();
     }
 }
--- Leetcode/src/com/st0ck53y/leetcode/six/SolutionTest.java
+++ Leetcode/src/com/st0ck53y/leetcode/six/SolutionTest.java
@@ -1,14 +1,40 @@
 package com.st0ck53y.leetcode.six;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SolutionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConvertPayNeg1() {
+		Solution solution = new Solution();
+		String expected = "PAYPALISHIRING";
+		String actual = solution.convert("PAYPALISHIRING", -1);
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testConvertPay1() {
+		Solution solution = new Solution();
+		String expected = "PAYPALISHIRING";
+		String actual = solution.convert("PAYPALISHIRING", 1);
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testConvertPay3() {
+		Solution solution = new Solution();
+		String expected = "PAHNAPLSIIGYIR";
+		String actual = solution.convert("PAYPALISHIRING", 3);
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testConvertPay4() {
+		Solution solution = new Solution();
+		String expected = "PINALSIGYAHRPI";
+		String actual = solution.convert("PAYPALISHIRING", 4);
+		Assert.assertEquals(expected, actual);
 	}
 
 }
